8015c688086a09426085bc434efdb196bbce1541
Althaaff <muhammadalthaf228@gmail.com>
2025-01-27 23:51:13 +0530

    add callToAction to to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,34 @@
+import { Button } from "flowbite-react";
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 flex flex-col justify-center">
+        <h2 className="text-2xl">Want to learn more about javascript?</h2>
+
+        <p className="text-gray-500 my-2">
+          Checkout these rescources with 100 Javascript Projects
+        </p>
+
+        <Button
+          gradientDuoTone="purpleToPink"
+          className="rounded-tl-xl rounded-bl-none"
+        >
+          <a
+            href="https://codedamn.com/challenge/100-days-of-frontend"
+            target="_blank"
+            rel="noopener noreferer"
+          >
+            100 Days Of Frontend
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img
+          className="rounded"
+          src="https://res.cloudinary.com/dbr73rpz9/image/upload/v1688631686/images/1_YQgaKfVzK-YpxyT3NYqJAg_zcvpgb.png"
+        />
+      </div>
+    </div>
+  );
+}
